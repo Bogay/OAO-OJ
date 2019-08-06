54889188f14f7a3ff6b5d83c78ef61acf27919f7
Uier <ap9940506@gmail.com>
2019-08-07 00:26:50 +0800

    add f*ck line break btn

--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,28 @@
-function submit() {
-    $.post('/', { 'script': editor.getValue() }, function(data) {
-        $('#info').html(data);
-    });
+var editor;
+
+window.onload = () => {
+	editor = ace.edit('editor');
 }
 
 function test() {
     $.post('/', { 'input_data': $('#input').val(), 'output_data': $('#output').val(), 'script': editor.getValue() }, function(data) {
         $('#info').html(data);
     });
+}
+
+function submit() {
+    $.post('/', { 'script': editor.getValue() }, function(data) {
+        $('#info').html(data);
+    });
+}
+
+function rmnewline() {
+	str = editor.getValue();
+	console.log('str befor: ' + str);
+	str = str.replace(/(\r\n|\r|\n)/g, ' ');
+	// str.replace(/\r/g, '');
+	console.log('str after: ' + str);
+	editor.setValue(str);
 }
 
 function scale_font(o) {
