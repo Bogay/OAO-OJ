9fd76c1c3adfe798b0030a06820c170bb703e2b6
Uier <ap9940506@gmail.com>
2019-08-06 23:23:27 +0800

    scale font

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,17 @@
 function submit() {
-    $.post("/", { "script": editor.getValue() }, function(data) {
-        $("#info").html(data);
+    $.post('/', { 'script': editor.getValue() }, function(data) {
+        $('#info').html(data);
     });
 }
 
 function test() {
-    $.post("/", { "input_data": $("#input").val(), "output_data": $("#output").val(), "script": editor.getValue() }, function(data) {
-        $("#info").html(data);
+    $.post('/', { 'input_data': $('#input').val(), 'output_data': $('#output').val(), 'script': editor.getValue() }, function(data) {
+        $('#info').html(data);
     });
+}
+
+function scale_font(o) {
+	var fontsize = o.value + 'px';
+	console.log('fontSize: ' + fontsize);
+    $('#editor').css('fontSize', fontsize);
 }
