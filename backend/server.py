7d0addf0eb5a101c74049dd4edeaa1ecb0ed1de7
AlaRduTP <alardutp37259418@gmail.com>
2019-08-10 14:58:57 +0800

    half done

--- backend/server.py
+++ backend/server.py
@@ -3,16 +3,12 @@ import pymongo
 from flask import Flask, request, render_template, jsonify, Response
 from flask_cors import CORS
 
+from oj_mongo import update_problem, get_all_problems, get_problem, add_problem
+
 # constants for path
 TMP_DIR = 'tmp'
-TEST_DIR = 'testdata'
 PROB_DIR = 'prob'
-
-# constants for db
-PROB_COL = 'problems'
-
-mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
-oj_db = mongo_client['OAO-OJ']
+TEST_DIR = 'testdatas'
 
 app = Flask(__name__)
 CORS(app)
@@ -22,72 +18,6 @@ def readAll(name):
         ret = f.read()
     return ret
 
-
-def update_problem(pid, data):
-    title = data.get('title')
-    content = data.get('content')
-
-    if not (pid and title and content):
-        return jsonify({'msg': 'missing data!'}), 404
-
-
-def get_all_problems():
-    ps_col = oj_db[PROB_COL]
-    ps = [*ps_col.find({}, {'_id': 0, 'pid': 1, 'title': 1})]
-
-    return jsonify(ps), 200
-
-
-def get_problem(pid):
-    ps_col = oj_db[PROB_COL]
-    prob = ps_col.find_one({'pid': pid})
-
-    if not prob:
-        return jsonify({'msg': f'pid {pid} not found!'}), 404
-
-    # read problem content
-    try:
-        with open(f'{PROB_DIR}/{pid}/prob.md') as f:
-            desc = f.read()
-    except e:
-        return jsonify({'msg': f'error when reading problem\'s content!'}), 404
-
-    prob = {
-        'pid': prob['pid'],
-        'title': prob['title'],
-        'desc': desc
-    }
-
-    return jsonify(prob), 200
-
-def add_problem(pid):
-    ps_col = mongo_client[PROB_COL]
-
-    if ps_col.find_one({'pid': pid}):
-        return jsonify({'msg': f'the problem ID {pid} exists'}), 400
-
-    # TODO: check for problem data
-
-    ppath = f'{PROB_DIR}/{pid}'
-
-    try:
-        os.makedirs(ppath)
-    except:
-        return jsonify({'msg': 'error when writing problem content!'}), 400
-
-    # write problem description
-    with open(f'{ppath}/prob.md', 'w') as f:
-        f.write(request.value.get('desc'))
-
-    # insert to db
-    ps_col.insert_one({
-        'pid': pid,
-        'title': request.vlaue.get('title')
-    })
-
-    # TODO: write testdata
-
-    return jsonify({'msg': f'problem {pid} has been created'}), 201
 
 @app.route('/problems', methods=['GET'])
 @app.route('/problems/<pid>', methods=['GET', 'POST', 'PUT'])
