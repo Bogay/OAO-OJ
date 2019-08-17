33896780d038f3f39892fedc3e5a31cb2f71a71d
Uier <ap9940506@gmail.com>
2019-08-17 22:32:38 +0800

    add vue-codemirror

---
+++ frontend/src/components/Editor.vue
@@ -0,0 +1,50 @@
+<template>
+  <codemirror v-model="code" :options="cmOption" v-bind:style="{ 'font-size': fontSize }"></codemirror>
+</template>
+
+<script>
+import { codemirror } from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+// language
+import 'codemirror/mode/python/python.js'
+// theme css
+import 'codemirror/theme/monokai.css'
+// require active-line.js
+import 'codemirror/addon/selection/active-line.js'
+// closebrackets
+import 'codemirror/addon/edit/closebrackets.js'
+// keyMap
+import 'codemirror/mode/clike/clike.js'
+import 'codemirror/addon/edit/matchbrackets.js'
+import 'codemirror/addon/comment/comment.js'
+import 'codemirror/addon/dialog/dialog.js'
+import 'codemirror/addon/dialog/dialog.css'
+import 'codemirror/addon/search/searchcursor.js'
+import 'codemirror/addon/search/search.js'
+import 'codemirror/keymap/sublime.js'
+
+export default {
+  name: 'Editor',
+  props: ['code', 'fontSize'],
+  data () {
+    return {
+      cmOption: {
+        autoCloseBrackets: true,
+        tabSize: 4,
+        styleActiveLine: true,
+        lineNumbers: true,
+        line: true,
+        mode: 'text/x-python',
+        theme: 'monokai',
+        keyMap: 'sublime'
+      }
+    }
+  },
+  components: {
+    codemirror
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- frontend/src/components/Pro.vue
+++ frontend/src/components/Pro.vue
@@ -16,58 +16,44 @@
         </div>
     </div>
     <div class="row">
+      <!-- left part -->
       <div class="col-6">
-        <b-tabs content-class="mt-3" fill>
-          <b-tab title="Description" active></b-tab>
-          <b-tab title="Submission"></b-tab>
-          <b-tab title="Discussion"></b-tab>
-        </b-tabs>
-      </div>
-        <!-- <div id="info-area" class="col-sm-6 d-flex flex-column">
-            <ul id="info-nav" class="nav nav-tabs">
-                <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#info">Description</a></li>
-                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#submission">Submission</a></li>
-                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#discussion">Discussion</a></li>
-            </ul>
-            <div id="info-content" class="tab-content flex-grow-1">
-                <div id="info" class="tab-pane active">
-                    <h1>Description</h1>
-                </div>
-                <div id="submission" class="tab-pane">
-                    <h1>Submission</h1>
-                </div>
-                <div id="discussion" class="tab-pane">
-                    <h1>Discussion</h1>
-                </div>
-            </div>
-        </div> -->
-      <div id="code-area" class="col-sm-6">
-        <div class="card">
-          <!-- Editor Style Setting -->
-          <div id="fontsize-select-box" class="card-header">
-            FontSize:
-            <b-form-select title="FontSize:" v-model="selected" :options="options" size="sm" class="fss"></b-form-select>
-          </div>
-          <div class="card-body d-flex">
-            <div id="editor" class="editor flex-grow-1"></div>
-          </div>
-        </div>
-        <div id="run" class="mt-3">
-          <b-tabs no-fade>
-            <b-tab title="Input" active>
-              <b-form-textarea v-model="input" placeholder="Input" rows="3" class="text-area"></b-form-textarea>
+        <b-card no-body>
+          <b-tabs content-class="m-0" card fill>
+            <b-tab title="Description" active class="pt-0" >
+              <h1>Hello</h1>
             </b-tab>
-            <b-tab title="Output">
-              <b-form-textarea v-model="output" placeholder="Output" rows="3" class="text-area"></b-form-textarea>
-            </b-tab>
+            <b-tab title="Submission" class="pt-0"></b-tab>
+            <b-tab title="Discussion" class="pt-0"></b-tab>
           </b-tabs>
-          <div class="mt-3">
-            <b-button-group>
-              <b-button variant="success">Success</b-button>
-              <b-button variant="info">Info</b-button>
-              <b-button variant="warning">Warning</b-button>
-            </b-button-group>
+        </b-card>
+      </div>
+      <!-- right part -->
+      <div class="col-6">
+        <!-- Editor-->
+        <div class="card">
+          <div class="card-header mb-0 pb-2">
+            <label>FontSize:</label>
+            <b-form-select v-model="selected" v-on:change="getSelectedItem" :options="options" size="sm" class="fss"></b-form-select>
           </div>
+          <!-- codemirror -->
+          <div class="card-body p-0" v-if="show"><Editor :code="source" :fontSize="fontSizeValue"></Editor></div>
+        </div>
+        <!--  -->
+        <b-tabs no-fade class="mt-3">
+          <b-tab title="Input" active class="p-0">
+            <b-form-textarea v-model="input" placeholder="Input" rows="3" class="text-area"></b-form-textarea>
+          </b-tab>
+          <b-tab title="Output" class="p-0">
+            <b-form-textarea v-model="output" placeholder="Output" rows="3" class="text-area"></b-form-textarea>
+          </b-tab>
+        </b-tabs>
+        <div class="mt-3 mb-3">
+          <b-button-group>
+            <b-button variant="success">Success</b-button>
+            <b-button variant="info">Info</b-button>
+            <b-button variant="warning">Warning</b-button>
+          </b-button-group>
         </div>
       </div>
     </div>
@@ -75,51 +61,42 @@
 </template>
 
 <script>
-import ace from '../../static/ace-builds/src-noconflict/ace.js'
-
-// var editor = ace.edit('editor')
-// editor.setOptions({
-//   theme: 'ace/theme/monokai',
-//   printMargin: false,
-//   useWrapMode: true,
-//   indentedSoftWrap: false
-// })
-
-// editor.session.setOptions({
-//   mode: 'ace/mode/python',
-//   newLineMode: 'unix',
-//   useSoftTabs: true,
-//   tabSize: 4,
-//   wrap: true
-// })
+import Editor from './Editor'
 
 export default {
   name: 'Pro',
   data () {
     return {
-      selected: null,
+      selected: 12,
       options: [
-        { value: null, text: '--' },
-        { value: '8', text: '8' },
-        { value: '10', text: '10' },
-        { value: '11', text: '11' },
-        { value: '12', text: '12' },
-        { value: '13', text: '13' },
-        { value: '14', text: '14' },
-        { value: '16', text: '16' },
-        { value: '18', text: '18' },
-        { value: '24', text: '24' },
-        { value: '32', text: '32' },
-        { value: '48', text: '48' }
+        { value: 8, text: '8' },
+        { value: 9, text: '9' },
+        { value: 10, text: '10' },
+        { value: 11, text: '11' },
+        { value: 12, text: '12' },
+        { value: 13, text: '13' },
+        { value: 14, text: '14' },
+        { value: 15, text: '15' },
+        { value: 16, text: '16' },
+        { value: 18, text: '18' },
+        { value: 24, text: '24' },
+        { value: 32, text: '32' },
+        { value: 48, text: '48' }
       ],
+      source: 'print(\'Hello, World!\')',
+      fontSizeValue: '12px',
+      show: true,
       input: '',
       output: ''
     }
+  },
+  components: {
+    Editor
   }
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss">
 .fss {
   width: 10vh;
 }
@@ -127,5 +104,9 @@ export default {
 .text-area {
   border-top-left-radius: 0px;
   border-top-right-radius: 0px;
+}
+
+.CodeMirror {
+  font-family: 'Monaco';
 }
 </style>
